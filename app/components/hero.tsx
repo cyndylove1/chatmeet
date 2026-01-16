da75b373aa9aa8b5d7e6db5b41bb44190fe06113
Nweke Cynthia Chinenye <nwekecynthia2015@gmail.com>
2026-01-16 02:26:33 -0800

    completed project

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
       <div className="relative z-10 flex items-center min-h-screen px-4 lg:px-20">
         <div className="lg:max-w-[60%] w-full">
           <AnimatedItem index={0} delay={0.05}>
-            <h1 className="text-white text-[40px] lg:text-[50px] md:text-[60px] font-[600] leading-[50px]">
+            <h1 className="text-white text-[40px] lg:text-[55px] md:text-[60px] font-[600] leading-[50px]">
               Dating Built on <br />
               <span className="text-[var(--primary)]">Trust.</span>
             </h1>
